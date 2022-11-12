da5f595cb573c6cabe591073e3ed8a9af819251b
joao-lucas-paes <joaozinhopaes12@gmail.com>
2022-11-11 22:45:09 -0400

    Jogador added

---
+++ CopaManager/src/model/Jogador.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Jogador extends Pessoa {
+
+    private int num;
+
+    Jogador(String nome, String cpf, Endereco endereco, String celular, int num, Time time) {
+        super(nome, cpf, endereco, celular);
+
+        this.num = num;
+
+        time.add(this);
+    }
+
+    @Override
+    public String toString() {
+        return this.getNome() + " - " + this.num;
+    }
+}
